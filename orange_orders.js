3942bd00b1ed1118c72979dcd370e852de25b0a0
hamletnscc <113032060+hamletnscc@users.noreply.github.com>
2022-10-04 13:20:54 -0300

    Create orange_orders.js

---
+++ orange_orders.js
@@ -0,0 +1,24 @@
+"use strict";
+/*
+Author:
+Date:
+Description:
+*/
+
+// DO NOT EDIT: The main function to house our program code 
+function main()
+{
+    // enter your code here
+    //input
+  
+    //process
+  
+  
+    //output
+}
+
+// DO NOT EDIT: Trigger our main function to launch the program
+if (require.main === module)
+{
+    main();
+}
